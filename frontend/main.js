660d7c87bd0cc6b9d35dabe7cf800da60de78d13
Damms <j.damms@hotmail.com>
2025-01-20 21:02:37 +1300

    add js code to call az function api

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVistitCount();
+});
+
+const functionApi = '';
+
+const getVistitCount = () => {
+    let count = 0;
+    fetch(functionApi).then(response => {
+        return response.json();
+    }).then(response => {
+        console.log('Website called function API.');
+        count = response.count;
+        document.getElementById('counter').innerText = count;
+    }).catch(err => {
+        console.error(err);
+    });
+    return count;
+}
